29be8c6085165733c4e48b2f3335e5d999c3c030
Martin Castro <martincastro.cm@gmail.com>
2020-01-20 13:19:33 -0300

    CORs support added

--- modules/apigw.tf
+++ modules/apigw.tf
@@ -31,3 +31,62 @@ resource "aws_api_gateway_deployment" "api_gateway_deployment" {
   rest_api_id = var.api_gateway_id
   stage_name  = var.stage_name
 }
+
+
+## adding CORs support ##
+
+resource "aws_api_gateway_method" "cors_method" {
+  count = length(var.api_gateway_resource_id) == 0 ? 1 : 0
+
+  rest_api_id   = var.api_gateway_id
+  resource_id   = aws_api_gateway_resource.api_gateway_resource[0].id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+# aws_api_gateway_integration.
+resource "aws_api_gateway_integration" "cors_integration" {
+  count = length(var.api_gateway_resource_id) == 0 ? 1 : 0
+
+  rest_api_id = var.api_gateway_id
+  resource_id = aws_api_gateway_resource.api_gateway_resource[0].id
+  http_method = aws_api_gateway_method.cors_method[0].http_method
+
+  type = "MOCK"
+
+  request_templates = {
+    "application/json" = "{ \"statusCode\": 200 }"
+  }
+}
+
+# aws_api_gateway_integration_response._
+resource "aws_api_gateway_integration_response" "cors_response" {
+  count = length(var.api_gateway_resource_id) == 0 ? 1 : 0
+
+  rest_api_id = var.api_gateway_id
+  resource_id = aws_api_gateway_resource.api_gateway_resource[0].id
+  http_method = aws_api_gateway_method.cors_method[0].http_method
+  status_code = 200
+
+  response_parameters = local.integration_response_parameters
+
+  depends_on = [aws_api_gateway_integration.cors_integration, aws_api_gateway_method_response.cors_method_response]
+}
+
+# aws_api_gateway_method_response._
+resource "aws_api_gateway_method_response" "cors_method_response" {
+  count = length(var.api_gateway_resource_id) == 0 ? 1 : 0
+
+  rest_api_id = var.api_gateway_id
+  resource_id = aws_api_gateway_resource.api_gateway_resource[0].id
+  http_method = aws_api_gateway_method.cors_method[0].http_method
+  status_code = 200
+
+  response_parameters = local.method_response_parameters
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  depends_on = [aws_api_gateway_method.cors_method]
+}
